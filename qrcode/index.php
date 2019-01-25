3a6caa12e36edd42fe637aa0d0c55eec5eaee23a
张欢 <xueren0126@qq.com>
2019-01-25 18:36:10 +0800

    Update index.php

--- qrcode/index.php
+++ qrcode/index.php
@@ -6,7 +6,7 @@
  *
  * 请求字段描述
  * 参数    说明                  类型                                 是否必需
- * c	content             二维码内容 如：http://www.demo.com	 	   是
+ * c	content             二维码内容 如：http://www.demo.com	 	 是
  * s	size                大小，每像素几个点 1,2,3,4,5,6....	     否 默认7
  * bc	backgroud_color     背景色&透明度 rgba 逗号分开	 	     否 默认白色 255255,255,0
  * fc	froreground_color   前景色&透明度 rgba 逗号分开	 	     否 默认黑色 0,0,0,0
